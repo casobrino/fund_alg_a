d6125e954ce2dd5059ab233f66f8fae8ff9e593b
Csobrino <casobrino_0@hotmail.com>
2022-02-14 14:13:36 -0600

    Se envio numero par y impar aplication

---
+++ oddOrPair.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+
+    numero = int(input("Ingresa un numero entero positivo: "))
+    if numero % 2 == 0:
+        print(f"{numero} Es un numero par")
+    else:
+        print(f"{numero} Es un numero inpar")
+
