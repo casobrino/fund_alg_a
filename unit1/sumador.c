4f8653656bfe0a52ecef21a4f13a48a63d5dce55
Cesar Sobrino <casobrino_0@hotmail.com>
2022-04-07 19:45:58 -0500

    programa del rectangulo agregado

--- unit1/sumador.c
+++ unit1/sumador.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
-int a,b;
-
 int main(){
+int a,b;
     printf("Ingresa numero1");
-    scanf("%d", &a);
+    scanf("Ingresa numero1,%d", &a);
     printf("Ingresa numero2");
     scanf("%d", &b);
     int sum= a + b;
